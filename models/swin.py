4005da15e9358566983bf45f64fac05aef36b598
X_SZU <117610848+Xiao-hb@users.noreply.github.com>
2025-05-11 20:00:55 +0800

    Update swin.py

--- models/swin.py
+++ models/swin.py
@@ -1,5 +1,3 @@
-# https://github.com/berniwal/swin-transformer-pytorch
-
 import torch
 from torch import nn, einsum
 import numpy as np
@@ -14,8 +12,6 @@ class CyclicShift(nn.Module):
         self.displacement = displacement
 
     def forward(self, x):
-        # 在第 1 维度上移动 self.displacement 位，在第 2 维度上移动 self.displacement 位
-        # 正数表示向下移动，负数表示向上移动
         return torch.roll(x, shifts=(self.displacement, self.displacement), dims=(1, 2))
 
 
@@ -76,38 +72,38 @@ def get_relative_distances(window_size):
 class WindowAttention(nn.Module):
     def __init__(self, dim, heads, head_dim, shifted, window_size, relative_pos_embedding):
         super().__init__()
-        inner_dim = head_dim * heads    # 输入多头子注意力时需要的数据维度
+        inner_dim = head_dim * heads    
 
         self.heads = heads
-        self.scale = head_dim ** -0.5   # 注意力公式里的分母，即缩放因子，注意：是 head_dim 而不是 inner_dim
+        self.scale = head_dim ** -0.5   
         self.window_size = window_size
         self.relative_pos_embedding = relative_pos_embedding
         self.shifted = shifted
 
         if self.shifted:
-            displacement = window_size // 2     # shift 量为窗口大小的一半
-            self.cyclic_shift = CyclicShift(-displacement)      # 实现窗口的循环偏移
-            self.cyclic_back_shift = CyclicShift(displacement)  # 恢复
+            displacement = window_size // 2     
+            self.cyclic_shift = CyclicShift(-displacement)      
+            self.cyclic_back_shift = CyclicShift(displacement)  
             self.upper_lower_mask = nn.Parameter(create_mask(window_size=window_size, displacement=displacement,
                                                              upper_lower=True, left_right=False), requires_grad=False)
             self.left_right_mask = nn.Parameter(create_mask(window_size=window_size, displacement=displacement,
                                                             upper_lower=False, left_right=True), requires_grad=False)
 
-        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias=False)     # 映射输入数据，得到 Q K V，注意：是 inner_dim*3 而不是 dim*3
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias=False)     
 
         if self.relative_pos_embedding:
-            self.relative_indices = get_relative_distances(window_size) + window_size - 1               # 计算相对位置索引
-            self.pos_embedding = nn.Parameter(torch.randn(2 * window_size - 1, 2 * window_size - 1))    # 初始化相对位置编码矩阵，大小为 (2M-1)*(2M-1)
+            self.relative_indices = get_relative_distances(window_size) + window_size - 1               
+            self.pos_embedding = nn.Parameter(torch.randn(2 * window_size - 1, 2 * window_size - 1))    
         else:
-            self.pos_embedding = nn.Parameter(torch.randn(window_size ** 2, window_size ** 2))      # 否则，初始化绝对位置编码矩阵
+            self.pos_embedding = nn.Parameter(torch.randn(window_size ** 2, window_size ** 2))      
 
-        self.to_out = nn.Linear(inner_dim, dim)     # 将多头自注意力的输出维度映射回输入维度
+        self.to_out = nn.Linear(inner_dim, dim)     
 
     def forward(self, x):
         if self.shifted:
-            x = self.cyclic_shift(x)    # 若对窗口进循环偏移，则为 SW-MSA
+            x = self.cyclic_shift(x)    
 
-        b, n_h, n_w, _, h = *x.shape, self.heads    # x.shape 返回一个包含张量维度的元组，使用 * 进行解包操作
+        b, n_h, n_w, _, h = *x.shape, self.heads    
 
         qkv = self.to_qkv(x).chunk(3, dim=-1)
         nw_h = n_h // self.window_size
@@ -117,7 +113,7 @@ class WindowAttention(nn.Module):
             lambda t: rearrange(t, 'b (nw_h w_h) (nw_w w_w) (h d) -> b h (nw_h nw_w) (w_h w_w) d',
                                 h=h, w_h=self.window_size, w_w=self.window_size), qkv)
 
-        dots = einsum('b h w i d, b h w j d -> b h w i j', q, k) * self.scale   # 使用 einsum 计算 Q 和 K 的点积
+        dots = einsum('b h w i d, b h w j d -> b h w i j', q, k) * self.scale   
 
         if self.relative_pos_embedding:
             dots += self.pos_embedding[self.relative_indices[:, :, 0], self.relative_indices[:, :, 1]]
@@ -160,21 +156,15 @@ class SwinBlock(nn.Module):
 class PatchMerging(nn.Module):
     def __init__(self, in_channels, out_channels, downscaling_factor):
         super().__init__()
-        '''
-        nn.Unfold 用于提取局部块，会将提取到的局部块进行 flatten，过程示意图可详见：https://blog.csdn.net/ViatorSun/article/details/119940759
-        view() 方法能够重新调整 Tensor 的形状，相当于 reshape； 若参数列表中的某一个参数定义为 -1，则表示自动调整对应维度上的元素个数，以保证总元素数不变
-            注意：view() 要求待处理的 tensor 在内存中连续存储，而 reshape() 则可以自动处理非连续的情况
-        permute(dims) 方法可以调整矩阵的维度顺序；0，1，2... 等表示第0维，第1维，第2维...'''
-        # 假设输入大小为 [b, c, h, w]
         self.downscaling_factor = downscaling_factor
-        self.patch_merge = nn.Unfold(kernel_size=downscaling_factor, stride=downscaling_factor, padding=0)  # 得到的大小为 [b, c * kernel_size**2, new_h * new_w]
+        self.patch_merge = nn.Unfold(kernel_size=downscaling_factor, stride=downscaling_factor, padding=0)  
         self.linear = nn.Linear(in_channels * downscaling_factor ** 2, out_channels)
 
     def forward(self, x):
         b, c, h, w = x.shape
         new_h, new_w = h // self.downscaling_factor, w // self.downscaling_factor
-        x = self.patch_merge(x).view(b, -1, new_h, new_w).permute(0, 2, 3, 1)   # 将通道维度调整到最后，便于后续线性层的映射处理，即 [b, new_h, new_w, c * kernel_size**2]
-        x = self.linear(x)  # 线性层默认作用在张量的最后一个维度
+        x = self.patch_merge(x).view(b, -1, new_h, new_w).permute(0, 2, 3, 1)   
+        x = self.linear(x)  
         return x
 
 
@@ -183,14 +173,11 @@ class StageModule(nn.Module):
                  relative_pos_embedding):
         super().__init__()
         assert layers % 2 == 0, 'Stage layers need to be divisible by 2 for regular and shifted block.'
-        # 例：layers == 2 表示包含 1 个 W-MSA 和 1 个 SW-MSA
-
         self.patch_partition = PatchMerging(in_channels=in_channels, out_channels=hidden_dimension,
                                             downscaling_factor=downscaling_factor)
 
         self.layers = nn.ModuleList([])
         for _ in range(layers // 2):
-            # 注： transformer block 的输入、输出维度相同
             self.layers.append(nn.ModuleList([
                 SwinBlock(dim=hidden_dimension, heads=num_heads, head_dim=head_dim, mlp_dim=hidden_dimension * 4,
                           shifted=False, window_size=window_size, relative_pos_embedding=relative_pos_embedding),
@@ -203,7 +190,7 @@ class StageModule(nn.Module):
         for regular_block, shifted_block in self.layers:
             x = regular_block(x)
             x = shifted_block(x)
-        return x.permute(0, 3, 1, 2)    # 将输出的维度从 [b, h, w, c] 调整为 [b, c, h, w]
+        return x.permute(0, 3, 1, 2) 
 
 
 class SwinTransformer(nn.Module):
@@ -234,14 +221,14 @@ class SwinTransformer(nn.Module):
                                 bias_init_mode=bias_init_mode, bias_init_mean=bias_init_mean)
 
     def forward(self, img):
-        x = self.stage1(img)    # 输入大小为 [b, c, h, w]
+        x = self.stage1(img)   
         x = self.stage2(x)
         x = self.stage3(x)
         x = self.stage4(x)
-        x = x.mean(dim=[2, 3])  # 计算在第2，3维度上的平均值，[b, c, h, w] → [b, c]
+        x = x.mean(dim=[2, 3]) 
 
         x = self.LN(x)
-        features = x  # 添加：获取 Swin-Transformer 学习到的特征
+        features = x 
         x = self.fc(x)
         return x, features
         # return self.mlp_head(x)
